e5527f1b5100f9036a4e6860b9910b768793a910
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-08-15 19:05:04 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const main= document.getElementById("main");
+
+//document.body.append(element);
+//main.id = "main";
+main.remove();
+const newHeader =document.createElement("h1")
+newHeader.id = "victory";
+newHeader.textContent = "namo is the champion";
